d6fe4bf310b9ad12c1b9e220e4c3872462adbadd
tangchuan <tangkthh@163.com>
2018-02-08 22:17:30 +0800

    删除所有内容

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import 'core-js/fn/object/assign';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/Main_v2';
-
-// Render the main component into the dom
-// 渲染在页面的dom上
-ReactDOM.render(<App />, document.getElementById('contentWrap'));
